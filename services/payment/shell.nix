d2be03adb97fab303b0ad043f2b65e23e47f2a6d
Adriaan Waem <2ndalgorithm@gmail.com>
2024-09-30 13:02:59 +0200

    enhance building scripts (and remove some)

--- services/payment/shell.nix
+++ services/payment/shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ python3 python312Packages.grpcio python312Packages.grpcio-tools ];
+    nativeBuildInputs = with pkgs.buildPackages; [ python312 python312Packages.grpcio python312Packages.grpcio-tools ];
 }
